5757b42710b5609a1020fad4f8b92901cdfb9754
Karan Nehra <karanehra@gmail.com>
2019-12-29 23:12:12 +0530

    Added cors handler

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"juno/router"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	const PORT = 3007
 	fmt.Printf("Server started on PORT:%d\n", PORT)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), router.SetupRouter()))
+	log.Fatal(
+		http.ListenAndServe(fmt.Sprintf(":%d", PORT),
+			handlers.CORS(
+				handlers.AllowedOrigins([]string{"*"}))(router.SetupRouter())))
 }
