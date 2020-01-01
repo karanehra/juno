1c5f7f21d97d8272691e0249143f76d46ce64ed7
Karan Nehra <karanehra@gmail.com>
2020-01-01 21:14:37 +0530

    Started work on feed API sets

---
+++ models/feed.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"context"
+	"juno/database"
+	"juno/generics"
+	"juno/util"
+	"net/http"
+	"time"
+)
+
+//Feed defines the model for a feed
+type Feed struct {
+	URL   string `json:"URL"`
+	Title string `json:"title"`
+}
+
+func (feed *Feed) Validate() []string {
+	return generics.GenericModelInstanceValidator(feed)
+}
+
+//CreateAndSendResponse adds a userInstance to DB and sends appropriate response
+func (feed *Feed) CreateAndSendResponse(res http.ResponseWriter) {
+	coll := database.DB.Collection("feeds")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	result, err := coll.InsertOne(ctx, feed)
+	if err != nil {
+		util.SendServerErrorResponse(res, err.Error())
+		return
+	}
+	util.SendSuccessCreatedResponse(res, result)
+}
